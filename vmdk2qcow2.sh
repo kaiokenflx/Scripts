d1147cb0c1a4fb110fc90a8a852c46b2a3123f0e
kaiokenflx <kaiokenflx@proton.me>
2026-04-22 18:41:23 +0200

    INITIAL COMMIT

---
+++ vmdk2qcow2.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [[ $# -eq 0 ]]
+then
+	echo "Please provide the path to the VMDK source file!"
+	exit 1
+fi
+
+VMDK="$1"
+
+if [[ "$(qemu-img info $VMDK |grep 'file format:')" != *"vmdk"* ]]
+then
+	echo "Error: Failed to verify the provided source file"
+	exit 1
+fi
+
+VMDK_BASENAME="${VMDK%.*}"
+QCOW_OUTPUT_FILENAME="$VMDK_BASENAME.qcow2"
+
+printf "Convert: '%s' => '%s'..\n" $VMDK $QCOW_OUTPUT_FILENAME
+
+qemu-img convert -cpf vmdk -O qcow2 $VMDK $QCOW_OUTPUT_FILENAME
+
+echo "Finished!"
